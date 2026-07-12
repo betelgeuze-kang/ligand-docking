691a7e79cd3e4a0fd1186a1fd36a27e96fe771d1
betelgeuze-kang <j59386@naver.com>
2026-07-12 23:01:52 +0900

    v2-a: export staged status and version taxonomy

--- betelgeuze_engine_v2/contracts/__init__.py
+++ betelgeuze_engine_v2/contracts/__init__.py
@@ -1,25 +1,49 @@
-"""Public contracts for the independent engine v2."""
+"""Public contracts for the independent Engine v2."""
 
 from .schema import (
     ALL_ATOM_SCHEMA_ID,
     ALL_ATOM_SCHEMA_NAME,
     ALL_ATOM_SCHEMA_VERSION,
+    CHECKPOINT_SCHEMA_VERSION,
+    DISTRIBUTION_NAME,
+    DISTRIBUTION_VERSION,
     ENGINE_API_VERSION,
+    ENGINE_RESULT_SCHEMA_VERSION,
+    RUNTIME_INPUT_SCHEMA_VERSION,
+    VERSION_TAXONOMY,
     ContractVersionError,
     SchemaIdentity,
     SemanticVersion,
+    VersionTaxonomy,
     parse_schema_id,
     require_compatible_schema,
+)
+from .status import (
+    UNCALIBRATED_ENERGY,
+    UNCALIBRATED_FORCE,
+    ClaimStage,
+    QuantityDescriptor,
 )
 
 __all__ = [
     "ALL_ATOM_SCHEMA_ID",
     "ALL_ATOM_SCHEMA_NAME",
     "ALL_ATOM_SCHEMA_VERSION",
+    "CHECKPOINT_SCHEMA_VERSION",
+    "DISTRIBUTION_NAME",
+    "DISTRIBUTION_VERSION",
     "ENGINE_API_VERSION",
+    "ENGINE_RESULT_SCHEMA_VERSION",
+    "RUNTIME_INPUT_SCHEMA_VERSION",
+    "VERSION_TAXONOMY",
+    "ClaimStage",
     "ContractVersionError",
+    "QuantityDescriptor",
     "SchemaIdentity",
     "SemanticVersion",
+    "UNCALIBRATED_ENERGY",
+    "UNCALIBRATED_FORCE",
+    "VersionTaxonomy",
     "parse_schema_id",
     "require_compatible_schema",
 ]
